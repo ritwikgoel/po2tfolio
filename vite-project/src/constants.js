c74f4a4e5a5f900130e41dabee928312e07c385b
Ritwik Goel <ritwikgoel@yahoo.com>
2024-05-16 21:49:36 -0400

    text + ui fixes

--- vite-project/src/constants.js
+++ vite-project/src/constants.js
@@ -1,13 +1,12 @@
 export const scaleFactor = 2.5;
 
 export const dialogueData = {
-  degree: `Degree `,
-  key:  `key`,
-  oldman:  `oldman`,
-  potion: `potion`,
-  bed: `bed`,
-  resume: `resume`,
-  projects: `projects here`,
-  chest: `chests`,
-  magic: `Key`,
+  degree: `This is my undergrad cs degree from VIT University. In December, when I complete my masters, I'll add a M.S in Computer Engineering from Columbia to the wall too! `,
+  oldman:  `A wise old man who says, 'The job market is only bad when you need a job.'`,
+  potion: `A bottle of melatonin to help me sleep after long hours of work.`,
+  bed: `My bed to rest and recover after a good workout.`,
+  resume: `Here you'll find my professional resume showcasing my skills and achievements. <link to resume>`,
+  projects: `Dive into my GitHub repository and explore a world of innovation and creativity. Check it out <a href="https://github.com/ritwikgoel" target="_blank">Here</a>.`,
+  chest: `You've found my chest of insights into me. Check out my YouTube channel <a href="https://www.youtube.com/channel/UCILKFL1XgAG61BYie1Egp-w" target="_blank">here</a> where I (very occasionally) make cooking videos and share my rollerblading adventures (still a work in progress).`,
+  magic: `I've hidden something here ( ˇ෴ˇ )`,
 };
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "./",
+    base: "/po2tfolio/",
     build: {
         minify: "terser",
     },
